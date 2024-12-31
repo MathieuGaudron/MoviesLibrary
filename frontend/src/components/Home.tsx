d536864e05d9984b46140592bc00090029a89501
mathieu <mathieu.gaudron@epitech.eu>
2024-12-31 12:16:51 +0100

    [ADD] Carousel home + respnsonsiv navbar

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,43 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Navbar from "./Navbar";
 import { Link } from "react-router-dom";
 
-const Home = () => {
+interface Movie {
+  id: number;
+  title: string;
+  poster_path: string;
+}
+
+const Home: React.FC = () => {
+  const [movie, setMovie] = useState<Movie[]>([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const API_KEY = "49c4a00d23ea0f1e33aac25430e1195d";
+  const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=fr-FR&page=1`;
+
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        setMovie(data.results);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des films :", error);
+      }
+    };
+    fetchMovies();
+  }, []);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === movie.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? movie.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <div className="relative h-screen overflow-y-auto">
 
-      {/* Navbar */}
-      <nav className="fixed top-0 left-0 right-0 w-full p-4 z-20 flex justify-between items-center bg-black shadow-lg">
-        <h1 className="text-3xl font-modak bg-gradient-to-r from-indigo-800 via-purple-300 to-indigo-800 bg-clip-text text-transparent transform transition duration-700 hover:scale-105">
-          <Link to="/" className="cursor-pointer">
-            MOVIES LIBRARY
-          </Link>
-        </h1>
-        <ul className="flex space-x-4 font-inter">
-          <li>
-            <Link
-              to="/login"
-              className="text-white hover:text-gray-300 font-semibold cursor-pointer"
-            >
-              Se connecter →
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/signup"
-              className="bg-purple-600 text-white px-4 py-2 hover:bg-purple-500 rounded-full font-semibold cursor-pointer"
-            >
-              S'inscrire →
-            </Link>
-          </li>
-        </ul>
-      </nav>
-
-
+      <Navbar />
 
       <div className="animate-slideInUpWithBlur">
 
-        {/* Image background accueil */}
-        <div className="relative w-full h-[60vh] z-0">
+        {/* Background image */}
+        <div className="relative w-full h-[75vh] z-0">
           <img
             src="/assets/bg-movieslibrary.jpg"
             alt="image_acceuil"
@@ -49,27 +60,124 @@ const Home = () => {
             }}
           />
           <div className="absolute inset-0 flex flex-col justify-center items-center text-center z-10">
-            <h2 className="text-2xl sm:text-3xl md:text-4xl text-white font-inter font-bold">
+            <h2 className="lg:text-4xl sm:text-3xl p-3  md:text-4xl text-white font-inter font-bold">
               Découvrez notre catalogue des meilleurs Films et SériesTV du
               moment !
             </h2>
           </div>
         </div>
 
-        {/* Section avec 3 divs */}
-        <div className="relative w-full h-auto py-10 flex flex-col sm:flex-row justify-center items-center gap-6">
-          <div className="w-[80%] sm:w-[30%] h-[200px] bg-purple-500 rounded-lg flex justify-center items-center text-white font-bold">
-            Div Rouge
-          </div>
-          <div className="w-[80%] sm:w-[30%] h-[200px] bg-purple-500 rounded-lg flex justify-center items-center text-white font-bold">
-            Div Rose
-          </div>
-          <div className="w-[80%] sm:w-[30%] h-[200px] bg-purple-500 rounded-lg flex justify-center items-center text-white font-bold">
-            Div Jaune
+
+
+        <div className="mt-20">
+
+          {/* Caroussel film populaire */}
+          <div className="py-10">
+            {movie.length > 0 && (
+              <div className="relative w-full max-w-4xl mx-auto flex items-center">
+                <div className="w-1/4 opacity-50 transform transition hover:scale-105">
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${
+                      movie[
+                        (currentIndex - 1 + movie.length) % movie.length
+                      ].poster_path
+                    }`}
+                    alt={
+                      movie[
+                        (currentIndex - 1 + movie.length) % movie.length
+                      ].title
+                    }
+                    className="w-full h-[200px] object-contain rounded-lg cursor-pointer"
+                    onClick={prevSlide}
+                  />
+                </div>
+                <div className="w-2/4 relative">
+                  <Link
+                    to={`/movie/${movie[currentIndex].id}`}
+                  >
+                    <div className="overflow-hidden rounded-lg">
+                      <img
+                        src={`https://image.tmdb.org/t/p/w500${movie[currentIndex].poster_path}`}
+                        alt={movie[currentIndex].title}
+                        className="w-full h-[300px] object-contain transition-transform duration-500 hover:scale-110 cursor-pointer"
+                      />
+                    </div>
+                  </Link>
+                  <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-60 text-white font-semibold font-inter text-xl text-center py-2">
+                    {movie[currentIndex].title}
+                  </div>
+                </div>
+                <div className="w-1/4 opacity-50 transform transition hover:scale-105">
+                  <img src={`https://image.tmdb.org/t/p/w500${movie[(currentIndex + 1) % movie.length].poster_path}`}
+                    alt={movie[(currentIndex + 1) % movie.length].title}
+                    className="w-full h-[200px] object-contain rounded-lg cursor-pointer"
+                    onClick={nextSlide}
+                  />
+                </div>
+
+                <button
+                    className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-black bg-opacity-60 text-white p-2 rounded-full z-10 hover:bg-opacity-80 transition"
+                    onClick={prevSlide}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={3}
+                    >
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
+                    </svg>
+                  </button>
+
+                  <button
+                    className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-black bg-opacity-60 text-white p-2 rounded-full z-10 hover:bg-opacity-80 transition"
+                    onClick={nextSlide}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={3}
+                    >
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+                    </svg>
+                </button>
+
+              </div>
+            )}
           </div>
         </div>
 
-        
+
+        <div className="relative w-full h-auto py-10 flex flex-col sm:flex-row justify-center items-center gap-28 mt-20">
+
+          {/* 3 Div */}
+          <div className="flex flex-col justify-center items-center text-center">
+            <div className="text-purple-500 text-5xl mb-4">
+              <i className="fas fa-film"></i> 
+            </div>
+            <p className="text-white text-lg font-semibold">Un divertissement <br /> sans fin.</p>
+          </div>
+
+          <div className="flex flex-col justify-center items-center text-center">
+            <div className="text-purple-500 text-5xl mb-4">
+              <i className="fas fa-tablet-alt"></i> 
+            </div>
+            <p className="text-white text-lg font-semibold">Disponible sur <br /> tous vos appareils.</p>
+          </div>
+
+          <div className="flex flex-col justify-center items-center text-center">
+            <div className="text-purple-500 text-5xl mb-4">
+              <i className="fas fa-bell"></i>
+            </div>
+            <p className="text-white text-lg font-semibold">Restez informés <br /> des dernières sorties.</p>
+          </div>
+
+        </div>
       </div>
     </div>
   );
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Navbar: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <nav className="sticky top-0 left-0 right-0 max-w-full w-full p-4 z-20 flex justify-between items-center bg-black shadow-lg">
+      {/* Logo */}
+      <h1 className="text-3xl font-modak bg-gradient-to-r from-indigo-800 via-purple-300 to-indigo-800 bg-clip-text text-transparent transform transition duration-700 hover:scale-105">
+        <Link to="/" className="cursor-pointer">
+          MOVIES LIBRARY
+        </Link>
+      </h1>
+
+      {/* Menu burger */}
+      <div className="sm:hidden">
+        <button
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          className="text-white focus:outline-none"
+        >
+          {isMenuOpen ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          )}
+        </button>
+      </div>
+
+      {/* Liens de navigation */}
+      <ul
+        className={`${
+          isMenuOpen ? "block" : "hidden"
+        } sm:flex space-x-4 font-inter sm:space-x-4 sm:items-center sm:static sm:gap-0 absolute sm:relative bg-black sm:bg-transparent top-14 sm:top-0 left-0 w-full sm:w-auto p-4 sm:p-0 z-10`}
+      >
+        <li className="my-2 sm:my-0">
+          <Link
+            to="/login"
+            className="block w-full sm:w-auto text-white hover:text-gray-300 font-semibold cursor-pointer text-center sm:text-left"
+          >
+            Se connecter →
+          </Link>
+        </li>
+        <li className="my-2 sm:my-0">
+          <Link
+            to="/signup"
+            className="block w-full sm:w-auto bg-purple-600 text-white px-4 py-2 hover:bg-purple-500 rounded-full font-semibold cursor-pointer text-center"
+          >
+            S'inscrire →
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
